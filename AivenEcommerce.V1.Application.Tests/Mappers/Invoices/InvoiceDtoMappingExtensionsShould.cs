07bfa603d47147c548a42b789e76b883f686dfed
lautaro <lautaroecarro@gmail.com>
2020-12-10 02:27:02 -0300

    tests invoice mapping extensions

---
+++ AivenEcommerce.V1.Application.Tests/Mappers/Invoices/InvoiceDtoMappingExtensionsShould.cs
@@ -0,0 +1,88 @@
+﻿using AivenEcommerce.V1.Application.Mappers.Invoices;
+using AivenEcommerce.V1.Domain.Entities;
+using AivenEcommerce.V1.Domain.Shared.Dtos.Invoices;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace AivenEcommerce.V1.Application.Tests.Mappers.Invoices
+{
+    public class InvoiceDtoMappingExtensionsShould
+    {
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSameId()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.Id, invoiceDto.Id);
+        }
+
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSameLink()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.Link, invoiceDto.Link);
+        }
+
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSameTransaction()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.Transaction, invoiceDto.Transaction);
+        }
+
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSameOrderId()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.OrderId, invoiceDto.OrderId);
+        }
+
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSamePaymentProviderWebhookReceived()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.PaymentProviderWebhookReceived, invoiceDto.PaymentProviderWebhookReceived);
+        }
+
+        [Fact]
+        public void ConvertToDto_InputNotNull_ReturnSamePaymentProvider()
+        {
+            Invoice invoice = MockInvoice();
+
+            InvoiceDto invoiceDto = invoice.ConvertToDto();
+
+            Assert.Equal(invoice.PaymentProvider, invoiceDto.PaymentProvider);
+        }
+
+        private Invoice MockInvoice() =>
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Transaction = nameof(Invoice.Transaction),
+            OrderId = nameof(Invoice.OrderId),
+            PaymentProviderWebhookReceived = true,
+            PaymentProvider = Domain.Shared.Enums.PaymentProvider.MercadoPago,
+            Link = new("http://contoso.com")
+        };
+    }
+}
