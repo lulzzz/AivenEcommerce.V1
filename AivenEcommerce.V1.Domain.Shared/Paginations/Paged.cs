d1e9cc154eab904bc659d5d1f92ccabb91e6dac2
lautaro <lautaroecarro@gmail.com>
2020-12-14 00:46:28 -0300

    remove Paged

--- AivenEcommerce.V1.Domain.Shared/Paginations/Paged.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace AivenEcommerce.V1.Domain.Shared.Paginations
-{
-    public record Paged(int CurrentPage, long TotalCount, long TotalPages, int PageSize);
-}
--- AivenEcommerce.V1.Domain.Shared/Paginations/PagedResult.cs
+++ AivenEcommerce.V1.Domain.Shared/Paginations/PagedResult.cs
@@ -1,10 +1,8 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace AivenEcommerce.V1.Domain.Shared.Paginations
 {
-    public record PagedResult<T>(int CurrentPage, long TotalCount, long TotalPages, int PageSize, IEnumerable<T> Items) : Paged(CurrentPage, TotalCount, TotalPages, PageSize)
+    public record PagedResult<T>(int CurrentPage, long TotalCount, long TotalPages, int PageSize, IEnumerable<T> Items)
     {
         public bool HasPrevious => CurrentPage > 1;
         public bool HasNext => CurrentPage < TotalPages;
